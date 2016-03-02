9ce8f8abe089857972a4c08bcf9f165502a0d590
Miguel Sampaio <sampaio.veiga@gmail.com>
2016-03-02 10:51:05 +0000

    removed smtp error

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,5 @@
 ActionMailer::Base.raise_delivery_errors = false
 ActionMailer::Base.smtp_settings = {  
 	address:              '192.168.1.215',
-	#port:                 110,
-	#authentication:       :ntlm,
-	#user_name:            '40234',
-	#password:             '3fbg8qpy4y',
 	enable_starttls_auto: false
 }
